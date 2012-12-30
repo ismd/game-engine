45c2499fc12bab7ffd8c3373d2467f55e9948ae7
Vladimir Kosteley <zzismd@gmail.com>
2012-12-31 00:43:44 +0700

    title для страницы инвентаря

--- application/controllers/WorldController.php
+++ application/controllers/WorldController.php
@@ -34,6 +34,8 @@ class WorldController extends AbstractAuthController {
      * Страница инвентаря
      */
     public function inventory() {
+        $this->view->setTitle('Инвентарь');
+
         $this->view->items = $this->session->character->items;
     }
 }
