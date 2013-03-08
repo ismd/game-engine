080207d26acab622bd5121fc5f800f3e6d03355f
Vladimir Kosteley <zzismd@gmail.com>
2013-03-08 15:44:19 +0700

    Перенёс логику в сервис

--- public/js/app/controllers/UserCtrl.js
+++ public/js/app/controllers/UserCtrl.js
@@ -14,7 +14,7 @@ function UserCtrl($scope, $http, CharactersList) {
                 $scope.user = data;
 
                 authForm.modal('hide');
-                CharactersList.showCharactersList();
+                CharactersList.show();
             } else {
                 alert('Не удалось войти');
                 loginButton.removeClass('disabled');
@@ -24,23 +24,9 @@ function UserCtrl($scope, $http, CharactersList) {
         });
     };
 
-    $scope.$on('showCharactersList', function() {
-        var selectCharacterForm = $('div#select-character');
-
-        selectCharacterForm.modal();
-        $scope.loadCharacters();
+    $scope.$on('characters-list-update', function(e, characters) {
+        $scope.characters = characters;
     });
-
-    $scope.loadCharacters = function() {
-        var loading = $('div#select-character').find('img.loading');
-
-        $http.get('/api/user/characters').success(function(data) {
-            loading.hide();
-            $scope.characters = data;
-        }).error(function() {
-            alert('Не удалось обратиться к серверу');
-        });
-    }
 }
 
 UserCtrl.$inject = ['$scope', '$http', 'CharactersList'];
--- public/js/app/services/CharactersList.js
+++ public/js/app/services/CharactersList.js
@@ -6,10 +6,20 @@ var windowServices = angular.module('windowServices', []);
 /**
  * Список персонажей пользователя
  */
-windowServices.factory('CharactersList', function($rootScope) {
+windowServices.factory('CharactersList', function($rootScope, $http) {
+    var selectCharacterForm = $('div#select-character');
+    var loading             = selectCharacterForm.find('img.loading');
+
     return {
-        'showCharactersList': function() {
-            $rootScope.$broadcast('showCharactersList');
+        'show': function() {
+            selectCharacterForm.modal();
+
+            $http.get('/api/user/characters').success(function(data) {
+                loading.hide();
+                $rootScope.$broadcast('characters-list-update', data);
+            }).error(function() {
+                alert('Не удалось обратиться к серверу');
+            });
         }
     };
 });
