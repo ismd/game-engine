691095909127a1c9399064eee006f0b8438c975c
Vladimir Kosteley <zzismd@gmail.com>
2013-03-09 23:29:23 +0700

    Подгрузка изображений карты

--- public/js/app/services/MapServices.js
+++ public/js/app/services/MapServices.js
@@ -8,8 +8,13 @@ mapServices.factory('Map', function($http, $rootScope) {
     var canvas = $('canvas#map');
     var ctx    = canvas.get(0).getContext('2d');
 
+    var image = new Image();
+
+    var player = new Image();
+    player.src = '/img/world/hero.png';
+
     // Test data
-    ctx.strokeRect(0, 0, 50, 50);
+    /*ctx.strokeRect(0, 0, 50, 50);
     ctx.strokeRect(50, 0, 50, 50);
     ctx.strokeRect(100, 0, 50, 50);
     ctx.strokeRect(150, 0, 50, 50);
@@ -43,13 +48,15 @@ mapServices.factory('Map', function($http, $rootScope) {
     ctx.strokeRect(150, 200, 50, 50);
     ctx.strokeRect(200, 200, 50, 50);
     ctx.strokeRect(250, 200, 50, 50);
-    ctx.strokeRect(300, 200, 50, 50);
+    ctx.strokeRect(300, 200, 50, 50);*/
 
     return {
         'init': function(init_idMap, init_x, init_y) {
             idMap = init_idMap;
             x     = init_x;
             y     = init_y;
+
+            updateImage();
         },
         'move': function(direction) {
             var newX = x;
@@ -81,6 +88,7 @@ mapServices.factory('Map', function($http, $rootScope) {
                     x = data.x;
                     y = data.y;
 
+                    updateImage();
                     $rootScope.$broadcast('move', x, y);
                 } else {
                     alert('Ошибка при перемещении');
@@ -90,4 +98,13 @@ mapServices.factory('Map', function($http, $rootScope) {
             });
         }
     };
+
+    function updateImage() {
+        image.src = '/img/world/maps/' + idMap + '/' + x + 'x' + y + '.png';
+
+        image.onload = function() {
+            ctx.drawImage(image, 0, 0);
+            ctx.drawImage(player, 150, 100);
+        };
+    }
 });
