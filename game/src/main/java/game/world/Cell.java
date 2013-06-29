683946ea5dc98cbc1d7692a63668c639910eb171
Vladimir Kosteley <zzismd@gmail.com>
2013-06-29 16:31:32 +0700

    CellType

--- game/src/main/java/game/world/Cell.java
+++ game/src/main/java/game/world/Cell.java
@@ -5,5 +5,11 @@ package game.world;
  */
 public class Cell {
 
+    private CellType type;
     private CellContent[] content;
+
+    Cell(CellType type) {
+        this.type = type;
+        System.out.println("CREATED NEW CELL OF TYPE " + type);
+    }
 }
