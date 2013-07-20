c220dc5b141a6e01eeff3dc72f7a571147ec8254
Vladimir Kosteley <zzismd@gmail.com>
2013-07-20 16:28:25 +0700

    Ресайз чата

--- site/public/js/app/init.js
+++ site/public/js/app/init.js
@@ -2,7 +2,7 @@
 
 angular.module('main', ['userService', 'characterService', 'registrationService',
     'characterCreateService', 'backgroundImageDirective', 'worldService',
-    'wsService', 'redirectorService'])
+    'wsService', 'redirectorService', 'autoResizableDirective'])
     .config(function($routeProvider, $locationProvider, $httpProvider) {
         $locationProvider.html5Mode(true);
 
