e86bd29722b9f93dd147a80e2a8a4469170b567c
Vladimir Kosteley <zzismd@gmail.com>
2013-08-04 19:13:54 +0700

    Обновление php-spa

--- site/public/index.php
+++ site/public/index.php
@@ -1,11 +1,9 @@
 <?php
-error_reporting(E_ALL ^ E_NOTICE ^ E_STRICT);
-
 // Определяем директорию с исходниками
 define('APPLICATION_PATH', realpath(dirname(__FILE__)) . '/../application');
 
-set_include_path(implode('/', array(
-    realpath(APPLICATION_PATH . '../lib'),
+set_include_path(implode(':', array(
+    realpath(APPLICATION_PATH . '/../lib'),
     get_include_path(),
 )));
 
