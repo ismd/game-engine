ef8f9b2e673d99796bc9f0ada4d4f5e33be5cc5f
Vladimir Kosteley <zzismd@gmail.com>
2013-12-02 02:27:55 +0700

    Вывод отправителя сообщения в чате

--- server/src/main/java/game/chat/ChatMessage.java
+++ server/src/main/java/game/chat/ChatMessage.java
@@ -5,16 +5,25 @@ import game.character.Character;
 import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
+import javax.persistence.Transient;
 
 /**
  * @author ismd
  */
 @Entity
 public class ChatMessage implements Serializable {
+
+    @ManyToOne(fetch=FetchType.EAGER) @JoinColumn(name="idSender", insertable=false, updatable=false) @Expose
+    Character sender;
+    @Expose @Transient
+    Character receiver;
 
     @Id @GeneratedValue(strategy=GenerationType.AUTO)
     private int id;
@@ -29,14 +38,34 @@ public class ChatMessage implements Serializable {
     }
 
     public ChatMessage(Character senderCharacter, Character receiverCharacter, String message) {
-        this.idSender = senderCharacter.getId();
+        sender = senderCharacter;
+        idSender = sender.getId();
 
-        if (null != receiverCharacter) {
-            this.idReceiver = receiverCharacter.getId();
+        receiver = receiverCharacter;
+        if (null != receiver) {
+            idReceiver = receiver.getId();
         }
 
         this.message = message;
         sended = new Date();
+    }
+
+    public Character getSender() {
+        return sender;
+    }
+
+    public ChatMessage setSender(Character character) {
+        sender = character;
+        return this;
+    }
+
+    public Character getReceiver() {
+        return receiver;
+    }
+
+    public ChatMessage setReceiver(Character character) {
+        receiver = character;
+        return this;
     }
 
     // Геттеры
