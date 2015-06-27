44d42929c6aede8e5b59e3caf5ae4fba078cf92f
Vladimir Kosteley <zzismd@gmail.com>
2015-06-27 20:16:21 +0600

    Мобильная версия, рефакторинг

--- site/js/app/services/WorldService.js
+++ site/js/app/services/WorldService.js
@@ -4,12 +4,12 @@
     window.mainModule.factory('World', ['Ws', function(Ws) {
         var service = {};
 
-        var ctx = null,
-            sprites = new Image(),
-            hero = new Image();
+        var ctx         = null,
+            cellsSprite = new Image(),
+            hero        = new Image();
 
-        sprites.src = '/img/world/cells.png';
-        hero.src = '/img/world/hero.png';
+        cellsSprite.src = '/img/world/cells.png';
+        hero.src        = '/img/world/hero.png';
 
         service.init = function() {
             ctx = $('.js-map').get(0).getContext('2d');
@@ -20,26 +20,18 @@
             });
         };
 
-        var CELL_WIDTH = 30,
-            CELL_HEIGHT = 30,
+        var CELL_WIDTH       = 30,
+            CELL_HEIGHT      = 30,
             CELLS_HORIZONTAL = 9,
-            CELLS_VERTICAL = 9,
-            HERO_WIDTH = 20,
-            HERO_HEIGHT = 20;
+            CELLS_VERTICAL   = 9,
+            HERO_WIDTH       = 20,
+            HERO_HEIGHT      = 20;
 
         service.drawVicinity = function(vicinity) {
-            if (null === ctx) {
-                setTimeout(function() {
-                    service.drawVicinity(vicinity);
-                }, 3000);
-
-                return;
-            }
-
             for (var x = 0; x < CELLS_HORIZONTAL; x++) {
                 for (var y = 0; y < CELLS_VERTICAL; y++) {
                     if (null === vicinity[x][y]) {
-                        ctx.drawImage(sprites,
+                        ctx.drawImage(cellsSprite,
                                       0,
                                       0,
                                       CELL_WIDTH,
@@ -51,7 +43,7 @@
                         continue;
                     }
 
-                    ctx.drawImage(sprites,
+                    ctx.drawImage(cellsSprite,
                                   vicinity[x][y]['x'] * CELL_WIDTH,
                                   vicinity[x][y]['y'] * CELL_HEIGHT,
                                   CELL_WIDTH,
@@ -63,11 +55,15 @@
                 }
             }
 
-            ctx.drawImage(hero,
-                          CELL_WIDTH * Math.floor(CELLS_HORIZONTAL / 2) + (CELL_WIDTH - HERO_WIDTH) / 2,
-                          CELL_HEIGHT * Math.floor(CELLS_VERTICAL / 2) + (CELL_HEIGHT - HERO_HEIGHT) / 2,
-                          HERO_WIDTH,
-                          HERO_HEIGHT);
+            drawHero();
+
+            function drawHero() {
+                ctx.drawImage(hero,
+                              CELL_WIDTH * Math.floor(CELLS_HORIZONTAL / 2) + (CELL_WIDTH - HERO_WIDTH) / 2,
+                              CELL_HEIGHT * Math.floor(CELLS_VERTICAL / 2) + (CELL_HEIGHT - HERO_HEIGHT) / 2,
+                              HERO_WIDTH,
+                              HERO_HEIGHT);
+            }
         };
 
         return service;
