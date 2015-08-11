f0884120a93855272954b5ce1680beb718e20e86
Vladimir Kosteley <zzismd@gmail.com>
2015-08-11 16:07:45 +0600

    Выход из админки

--- admin/application/controllers/AuthController.php
+++ admin/application/controllers/AuthController.php
@@ -9,4 +9,14 @@ class AuthController extends PsController {
         $this->view->setLayout('empty');
         $this->getHelper('index')->init($this->registry, $this->view);
     }
+
+    public function logoutAction() {
+        unset($_COOKIE['user_id']);
+        unset($_COOKIE['user_authKey']);
+
+        setcookie('user_id', '', time() - 3600, '/');
+        setcookie('user_authKey', '', time() - 3600, '/');
+
+        $this->getHelper('redirector')->redirect('/');
+    }
 }
