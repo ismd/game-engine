20ff54b63a55e878ad2560eb04e4d05b9e7dd3c5
Vladimir Kosteley <zzismd@gmail.com>
2015-08-12 12:59:31 +0600

    stop.sh fix

--- bin/stop.sh
+++ bin/stop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-kill `ps -aux | grep ismd | grep 'start_game\|game.server.Main' | grep -v grep | cut -d ' ' -f 6 | tr '\n' ' '`
+kill `ps -aux | grep ismd | grep 'start_game\|game.server.Main' | grep -v grep | tr -s ' ' | cut -d ' ' -f 6 | tr '\n' ' '`
